8cf577b4830a5dcebac85d7ce7162911e64071a1
Jacobo <jacoboqc@yahoo.es>
2015-05-01 19:49:09 +0200

    Gets IP through InetAddress methods

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.Enumeration;
 import java.util.Scanner;
 
 
@@ -9,13 +12,21 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		String IP=null;
+		NetworkInterface interfaz = null;
 		try {
-			IP = InetAddress.getLocalHost().getHostAddress();
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
+			interfaz = NetworkInterface.getByName("wlan0");
+		} catch (SocketException e1) {
+			e1.printStackTrace();
 		}
-		String nombreFich="ripconf-" + IP;
+		Enumeration<InetAddress> enumDirs = interfaz.getInetAddresses();
+		String IP = null, IPt = null;
+		while(enumDirs.hasMoreElements()){
+			IPt = enumDirs.nextElement().getHostAddress();
+			if(IPt.startsWith("192")){
+				IP=IPt;
+			}
+		}
+		String nombreFich="ripconf-" + IP + ".txt";
 		try {
 			Scanner lectura= new Scanner(new FileInputStream(nombreFich));
 		} catch (FileNotFoundException e) {
