27fdada4c2ca8b70ed99e14e0a2b283d77659f5f
Jacobo Quintáns <jacoboqc@yahoo.es>
2015-05-05 15:29:14 +0200

    Added toString method

--- src/Router.java
+++ src/Router.java
@@ -1,7 +1,6 @@
 import java.io.Serializable;
 
-
-public class Router implements Serializable{
+public class Router implements Serializable {
 
 	/**
 	 * 
@@ -11,41 +10,55 @@ public class Router implements Serializable{
 	private String destino;
 	private String ruta;
 	private int prefijo;
-	
-	public Router(String destino, int distancia, String ruta, int prefijo){ 
+
+	public Router(String destino, int distancia, String ruta, int prefijo) {
 		this.distancia = distancia;
-		this.destino=destino;
-		this.ruta=ruta;
-		this.prefijo=prefijo;
+		this.destino = destino;
+		this.ruta = ruta;
+		this.prefijo = prefijo;
 	}
-	
-	
-	
-	public int getDistancia(){
+
+	public int getDistancia() {
 		return this.distancia;
 	}
-	public String getDestino(){
+
+	public String getDestino() {
 		return this.destino;
 	}
-	public String getRuta(){
+
+	public String getRuta() {
 		return this.ruta;
 	}
 
 	public int getPrefijo() {
 		return prefijo;
 	}
-	
-	public void setDistancia(int distancia){
-		 this.distancia= distancia;
+
+	public void setDistancia(int distancia) {
+		this.distancia = distancia;
 	}
-	
-	public void setDestino(String destino){
-		 this.destino= destino;
+
+	public void setDestino(String destino) {
+		this.destino = destino;
 	}
-	public void setRuta(String ruta){
-			this.ruta=ruta;
+
+	public void setRuta(String ruta) {
+		this.ruta = ruta;
 	}
+
 	public void setPrefijo(int prefijo) {
 		this.prefijo = prefijo;
 	}
+
+	@Override
+	public String toString() {
+		String string = this.destino;
+		if(this.prefijo!=0){
+			string+="/" + this.prefijo;
+		}
+		string+=" - " +this.ruta + " - " + this.distancia; 
+		return string;
+	}
+	
+	
 }
